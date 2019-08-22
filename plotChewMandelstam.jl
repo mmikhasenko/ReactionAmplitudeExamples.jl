e36eec780fe5d18e047f8c8259e9747a99c496f6
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-08-22 13:14:16 +0200

    Added threshold indication

--- plotChewMandelstam.jl
+++ plotChewMandelstam.jl
@@ -18,9 +18,9 @@ PyCall.PyDict(matplotlib["rcParams"])["text.latex.unicode"] = true
 ChewMandestam(0.77^2+1e-5im, 0.15^2, 0.17^2)
 iRho(0.77^2+1e-5im, 0.15^2, 0.17^2)
 
-let sv = LinRange(-0.1, 0.5,200), m1=0.14, m2=0.547
-    cal1 = ChewMandestam.(sv .+ 1e-4im, 0.15^2, 0.17^2)
-    cal2 = iRho.(sv .+ 1e-4im, 0.15^2, 0.17^2)
+let sv = LinRange(-0.1, 1.4, 200), m1=0.14, m2=0.547
+    cal1 = ChewMandestam.(sv .+ 1e-4im, m1^2, m2^2)
+    cal2 = iRho.(sv .+ 1e-4im, m1^2, m2^2)
     plot(layout=grid(1,2), size=(900,350), ylim=(-1,1), grid=false, frame=:frame)
     plot!(sv,  [real.(cal2) imag.(cal2)], sp=1,
         ylab=L"16\pi\,\,i\rho(s)", xlab=L"s\,\,(\mathrm{GeV}^2)",
@@ -30,6 +30,12 @@ let sv = LinRange(-0.1, 0.5,200), m1=0.14, m2=0.547
         ylab=L"16\pi\,\,\Sigma(s)", xlab=L"s\,\,(\mathrm{GeV}^2)",
         lab=[L"\mathrm{Real\,\,part}" L"\mathrm{Imag\,\,part}"],
         ls=[:dash :solid], lc=[:black :red], leg=:bottomright, lw=1.5)#, lc=[palette(:auto)[1] palette(:auto)[3]])
+    for sp=1:2
+        vline!(sp=sp, [(m1+m2)^2], lab="", l=(1,0.5,:grey),
+            ann=((m1+m2)^2, 0.5, text(L"s_{\mathrm{thr}}",:right,rotation=90)))
+        hline!(sp=sp, [0], lab="", l=(1,0.5,:grey))
+    end
+    plot!()
 end
 
 savefig(joinpath("plot","ChewMandelstam.pdf"))
