00e10916865bcd16d5dad0acabab3ac4c7c75035
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-08-12 00:57:07 +0200

    dispatch on wigner rotations

--- omegapipi.jl
+++ omegapipi.jl
@@ -4,7 +4,8 @@ using ThreeBodyDecay
 using Parameters
 using Plots
 
-theme
+theme(:wong2, frame=:box, grid=false, minorticks=true, 
+    guidefontvalign=:top, guidefonthalign=:right)
 
 const jω = 1
 const λω = 1
@@ -38,18 +39,98 @@ end
 
 # ϵWignerD(1,1,1,1, 0.3,0.3,0.3)
 
+
+H(j1,λ1,j2,λ2,j,l,s) = 
+    clebschgordan(j1,λ1,j2,λ2,s,λ1+λ2) *
+        clebschgordan(l,0,s,λ1+λ2,j,λ1+λ2)
+# HH(j1,j2,j3,j,l,L,j0) =
+#     clebschgordan(j1,λ1,j2,λ2,s,λ1+λ2) *
+#         clebschgordan(l,0,s,λ1+λ2,j,λ1+λ2)
+#     clebschgordan(j,τ,j3,λ3,S,τ+λ3) *
+#         clebschgordan(L,0,S,τ+λ3,j0,τ+λ3)
+
+
 #############################################################
+
+# with respect to the chain 1
+cosζ_for0_sign(k, σs, msq) = cosθhatk1(k, σs, msq), (k==2 ? :ACW : :CW)
+
+cosζ_fori_sign(k, σs, msq) = cosζ(k,σs, msq), (k==2 ? :ACW : :CW)
+cosζ_forj_sign(k, σs, msq) = cosζ(k,σs, msq), (k==2 ? :ACW : :CW)
+cosζ_fork_sign(k, σs, msq) = cosζ(k,σs, msq), (k==2 ? :ACW : :CW)
+
+abstract type IndexWignerRotation end
+struct TrivialWR <: IndexWignerRotation end
+struct HatWR{S} <: IndexWignerRotation
+    k::Int
+end
+struct ZetaRepWR{T,S} <: IndexWignerRotation
+    k::Int
+end
+struct ZetaAllWR{S} <: IndexWignerRotation
+    k::Int
+end
+
+ispositive(wr::Trivial) = true
+ispositive(wr::HatWR{S}) where S = S
+ispositive(wr::ZetaRepWR{T,S}) where {T,S} = S
+ispositive(wr::ZetaAllWR{S}) where S = S
+
+# ind("^0_{1(1)}") -> Trivial()
+# # 
+# ind("_{1(2)}^0") -> Hat{true}(1)
+# ind("_{2(1)}^0") -> Hat{false}(1)
+# # 
+# ind("_{2(1)}^1") -> ZetaRep{:S,true}(1)
+# ind("_{2(1)}^2") -> ZetaRep{:D,true}(2)
+# #
+# ind("_{1(2)}^3") -> ZetaAll{true}(3)
+# ind("_{2(1)}^3") -> ZetaAll{false}(3)
+
+seq(i,j) = (j-i) ∈ (1,-2)
+function wr(system_a, reference_b, particle_c=0)
+    system_a == reference_b && return TrivialWR()
+    S = seq(system_a, reference_b)
+    A,B = S ? (system_a, reference_b) : (reference_b, system_a)
+    # 
+    particle_c == 0 && return Hat{S}(B)
+    #
+    particle_c ∉ (system_a, reference_b) && return ZetaAll{S}(particle_c)
+    #
+    T = (particle_c == reference_b) ? :S : :D
+    return ZetaRep{T,!(S)}(particle_c)
+end
+
+wr(3,1)
+wr(1,2)
+# 
+wr(1,1,2)
+wr(1,2,2)
+wr(1,2,1)
+wr(1,3,2)
+wr(2,1,2)
+wr(2,1,1)
+wr(1,2,3)
 
 struct Chain{T}
     k::Int
     lineshape::T
-    j::Int
+    jR::Int
     # 
     j0::Int
     P::Int
     M::Int
     ϵ::Int
+    # 
+    L::Int
+    S::Int
+    l::Int
+    s::Int
 end
+
+wignerd_sign(j,λ1,λ2, cosθ, ispositive) =
+    (ispositive ? 1 : x"-1"^*(λ1-λ2)) *
+        wignerd(j,λ1,λ2, cosθ)
 
 """
 Decay matrix element for  X → ω π⁻ π⁰
@@ -57,18 +138,37 @@ Decay matrix element for  X → ω π⁻ π⁰
 function O(chain::Chain, λ, τ, v)
     @unpack σ1, σ2 = v
     @unpack lineshape, k = chain
-    @unpack j0, j = chain
+    @unpack j0, jR = chain
+    i,j = ij_from_k(k)
     #
-    _σs = Invariants(ms; σ1, σ2)
-    _cosθhat = cosθhatk1(k,_σs, ms^2)
-    _cosθij = cosθij(k, _σs, ms^2)
-    _σk = _σs[k]
-    # 
+    σs = Invariants(ms; σ1, σ2)
+    js = (jω,0,0)
+    λs = ( τ,0,0)
+    #
+    cosθ = cosθij(k, σs, ms^2)
+    #
+    w0 = wr(k,1,0); cosζ0 = cosζ(w0, σs, ms^2)
+    wi = wr(k,1,i); cosζi = cosζ(wi, σs, ms^2)
+    wj = wr(k,1,j); cosζj = cosζ(wj, σs, ms^2)
+    wk = wr(k,1,k); cosζk = cosζ(wk, σs, ms^2)
+    #
     angular = sum(
-        wignerd(j0,λ,ν, _cosθhat)*
-        wignerd(j, ν,τ, _cosθij ) for ν in -j:j)
+        wignerd_sign(j0,λ,ν-λs′[k], cosζ0, ispositive(w0)) *
+        #
+            H(j,τ,js[k],λs′[k],jR,L,S) * x"-1"^(js[k]-λs′[k]) *
+        wignerd(j, ν, τ, cosθ) *
+            H(js[i],λs′[i],js[j],λs′[j],jR,l,s) * x"-1"^(js[j]-λs′[j]) *
+        #
+        wignerd_sign(js[i],λs′[i],λs[i], cosζi, ispositive(wi)) *
+        wignerd_sign(js[j],λs′[j],λs[j], cosζj, ispositive(wj)) *
+        wignerd_sign(js[k],λs′[k],λs[k], cosζk, ispositive(wk))
+# 
+                for ν in -jR:jR,
+                    λs′ in Iterators.product(-js[1]:js[1],
+                                             -js[2]:js[2],
+                                             -js[3]:js[3]))
     # 
-    bw = lineshape(_σk)
+    bw = lineshape(σs[k])
     return angular*bw
 end
 
