b1e490c3a456ada1739db6ab8d82a45e52e7fe70
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-05-17 12:52:43 +0200

    add: dalitz projections

--- LcLcK_momentumdistribution.jl
+++ LcLcK_momentumdistribution.jl
@@ -17,6 +17,7 @@ begin
 	using ThreeBodyDecay
 	using Plots
 	using LaTeXStrings
+	using QuadGK
 end
 
 # ╔═╡ 9e8a48f6-9e5b-4907-81cd-cabc1ac28213
@@ -39,6 +40,24 @@ end ;
 
 # ╔═╡ adfc0267-f6e6-412d-80db-3ee0c73bc32b
 ms = ThreeBodyMasses(mΛc, mΛc, mK; m0=mB)
+
+# ╔═╡ 0f28d98f-b7ec-4f98-abd7-017b5b5e4641
+function project1(f, σ1, ms)
+	function integrand(σ2)
+		σs = Invariants(ms; σ1, σ2)
+		return inphrange(σs,ms) ? f(σs) : 0.0
+	end
+	quadgk(integrand, lims2(ms)...)[1]
+end
+
+# ╔═╡ e2f48768-b995-4260-9e19-a1764af3082e
+function project3(f, σ3, ms)
+	function integrand(σ2)
+		σs = Invariants(ms; σ2, σ3)
+		return inphrange(σs,ms) ? f(σs) : 0.0
+	end
+	quadgk(integrand, lims2(ms)...)[1]
+end
 
 # ╔═╡ a802e131-2c78-4c3f-9d9f-49aa45f261ac
 plot(map(x->sqrt.(x), border12(ms)), lc=:black, aspectratio=1,
@@ -94,21 +113,39 @@ md"""
 """
 
 # ╔═╡ 249d37cd-6abf-4f84-91c2-307b4f7441a8
-ϵP(p; σ=30e-3) = 1/(1+exp(-p/σ)) ;
+ϵP(p; σ=50e-3) = 1/(1+exp(-p/σ)) ;
 
 # ╔═╡ b42b89f8-7c7a-4de0-9914-a6dd95d2735f
-plot(ϵP, 0, pKmax,
-	ylab=L"\varepsilon",
-	xlab=L"P_{B\,\,\mathrm{rest}}(K)\,\,(\mathrm{MeV})")
+begin
+	plot(ϵP, 0, pKmax,
+		ylab=L"\varepsilon",
+		xlab=L"P_{B\,\,\mathrm{rest}}(K)\,\,(\mathrm{MeV})")
+	savefig("assumed_e_pK.pdf")
+	plot!()
+end
 
 # ╔═╡ 4f0e98ab-0070-49ee-97d0-3c1a293922ab
 ϵ(σs) = ϵP(pKofσ(σs[3])) ;
 
 # ╔═╡ b2066aab-7281-4a61-9fd1-b7862eea9fa4
-plot(ϵ, ms, clim=(0,1), aspectratio=1,
-	title="\$p_K\$ related (in)efficiency",
-	xlab=L"m(\Lambda_c^+ K^+)\,\,(\mathrm{MeV})",
-	ylab=L"m(\overline{\Lambda}_c^- K^+)\,\,(\mathrm{MeV})")
+let
+	plot(ϵ, ms, clim=(0,1), aspectratio=1,
+		title="\$p_K\$ related (in)efficiency",
+		xlab=L"m(\Lambda_c^+ K^+)\,\,(\mathrm{MeV})",
+		ylab=L"m(\overline{\Lambda}_c^- K^+)\,\,(\mathrm{MeV})")
+	savefig("e_on_dalitz.pdf")
+	plot!()
+end
+
+# ╔═╡ 61132c50-1336-4694-8182-e6ca68e8ade6
+let
+	plot(e1->project1(ϵ, e1^2, ms) / project1(σs->1.0, e1^2, ms),
+		sqrt.(lims1(ms))..., ylim=(0,1),
+		xlab=L"m(\overline{\Lambda}_c^- K^+)\,\,(\mathrm{MeV})",
+		ylab=L"\epsilon_\mathrm{ph.sp.}")
+	savefig("projected1_e.pdf")
+	plot!()
+end
 
 # ╔═╡ Cell order:
 # ╟─9e8a48f6-9e5b-4907-81cd-cabc1ac28213
@@ -116,6 +153,8 @@ plot(ϵ, ms, clim=(0,1), aspectratio=1,
 # ╠═8b888bf9-7a8f-45e8-b268-41e9fdd8901e
 # ╠═f9e829e0-6b08-403d-ba18-697ac51edfad
 # ╠═adfc0267-f6e6-412d-80db-3ee0c73bc32b
+# ╟─0f28d98f-b7ec-4f98-abd7-017b5b5e4641
+# ╟─e2f48768-b995-4260-9e19-a1764af3082e
 # ╟─a802e131-2c78-4c3f-9d9f-49aa45f261ac
 # ╟─bebec78c-ccd6-4336-9a05-67bb8f2a985c
 # ╠═9abc515b-d066-478d-8f1b-32bffadab34c
@@ -129,6 +168,7 @@ plot(ϵ, ms, clim=(0,1), aspectratio=1,
 # ╠═5eeb2014-1e63-4956-a60d-b21a076560c2
 # ╟─03bf0964-e32a-48d5-8f16-43bb0892762a
 # ╠═249d37cd-6abf-4f84-91c2-307b4f7441a8
-# ╟─b42b89f8-7c7a-4de0-9914-a6dd95d2735f
+# ╠═b42b89f8-7c7a-4de0-9914-a6dd95d2735f
 # ╠═4f0e98ab-0070-49ee-97d0-3c1a293922ab
 # ╟─b2066aab-7281-4a61-9fd1-b7862eea9fa4
+# ╟─61132c50-1336-4694-8182-e6ca68e8ade6
