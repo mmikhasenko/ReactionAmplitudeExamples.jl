0f2cfda8926cf741b71d39ce766155e81d23e7e5
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-03-29 18:39:58 +0200

    Fixes limit case for (alpha,theta) representation

--- circledalitz.jl
+++ circledalitz.jl
@@ -31,14 +31,41 @@ theme(:wong2, frame=:box, grid=false, minorticks=true,
     xlim=(:auto, :auto), ylim=(:auto, :auto),
     lw=1.2, lab="", colorbar=false)
 
-# ╔═╡ 99a30a66-2ae7-4914-9e68-b97f95dea378
-function rθdalitz(σs, ms)
-    thresholds = ((ms.m2 + ms.m3), (ms.m3 + ms.m1), (ms.m1 + ms.m2))
+# ╔═╡ f59fbbff-8470-4926-89c3-14dd5ab33ee8
+ms = ThreeBodyMasses(3, 1, 9; m0=14)
+
+# ╔═╡ 823b5793-8c1a-4277-95ed-0f84faf5f088
+function alwaysin(ms)
+	@unpack m0, m1, m2, m3 = ms
+	# 
+	σ1 = ((m2+m3+m0-m1)/2)^2
+	σ3 = σ3of1(0.1,σ1,ms^2)
+	σs2 = Invariants(ms; σ1, σ3)
+	# 
+	σ2 = ((m3+m1+m0-m2)/2)^2
+	σ1 = σ1of2(0,σ2,ms^2)
+	σs3 = Invariants(ms; σ2,σ1)
+	# 
+	σ3 = ((m1+m2+m0-m3)/2)^2
+	σ2 = σ2of3(0,σ3,ms^2)
+	σs1 = Invariants(ms; σ3,σ2)
+	
+	σs = ThreeBodyDecay.MandestamTuple(sum((σs1, σs2, σs3) .|> collect) / 3)
+	[σs1, σs2, σs3, σs]
+end
+
+# ╔═╡ fc40eced-a66e-4e08-ad1c-9a5dc39d4ef5
+function σs2rθ(σs, ms)
+	#
+	thresholds = ((ms.m2 + ms.m3), (ms.m3 + ms.m1), (ms.m1 + ms.m2))
     T0 = sum(abs2, ms) .- sum(abs2, thresholds)
+	#
+	σs0 = alwaysin(ms)[end]
+	Δσs0 = Tuple(σs0) .- thresholds .^ 2
     Δσs = Tuple(σs) .- thresholds .^ 2
     # 
-    rcosθ = T0 / 3 - Δσs[1]
-    rcosθmπ3 = Δσs[3] - T0 / 3
+    rcosθ = Δσs0[1] - Δσs[1]
+    rcosθmπ3 = Δσs[3] - Δσs0[3]
     rsinθ = 2 / √3 * (rcosθmπ3 - rcosθ / 2)
     # 
     θ = atan(rsinθ, rcosθ)
@@ -47,14 +74,36 @@ function rθdalitz(σs, ms)
     return (; θ, r)
 end
 
-# ╔═╡ f59fbbff-8470-4926-89c3-14dd5ab33ee8
-ms = ThreeBodyMasses(2, 2, 6; m0=14)
+# ╔═╡ dc1b8aa0-1cd6-44ce-9e3f-0d56bfc269ac
+function fixedborder(ms::ThreeBodyDecay.MassTuple; Nx::Int=300)
+    thresholds = ((ms.m2 + ms.m3), (ms.m3 + ms.m1), (ms.m1 + ms.m2))
+    T0 = sum(abs2, ms) .- sum(abs2, thresholds)
+
+    # 
+	σs0 = alwaysin(ms)[end]
+	Δσs0 = Tuple(σs0) .- thresholds .^ 2
+	# 
+	Ps(θ) = 
+	    Polynomial([Δσs0[1], -cos(θ)]),
+	    Polynomial([Δσs0[2], cos(θ + π / 3)]),
+	    Polynomial([Δσs0[3], cos(θ - π / 3)])
+    σs_P(θ) = thresholds .^ 2 .+ Ps(θ)
+	# 
+	ϕ = Base.Fix2(Kibble, ms^2)
+	rborder(θ) = minimum(filter(x -> x > 0, roots(ϕ(σs_P(θ)))))
+    σsborder(θ) =
+        map(σs_P(θ)) do P
+            P(rborder(θ))
+        end
+    θs = range(-π, π, length=Nx)
+    return ThreeBodyDecay.MandestamTuple.(σsborder.(θs))
+end
 
 # ╔═╡ 4bf79a8c-8f2e-4561-b886-1964ba95a11b
-σsv = border(ms)
+σsv = fixedborder(ms)
 
 # ╔═╡ ce4ca0e9-ee3f-47fc-a56c-570c33d61a9a
-rθv = rθdalitz.(σsv, Ref(ms))
+rθv = σs2rθ.(σsv, Ref(ms))
 
 # ╔═╡ 947586cf-63a9-4d5e-9caf-a175e3ec8bb2
 plot(
@@ -67,9 +116,18 @@ function rlimsdalitz(θ, ms)
     thresholds = ((ms.m2 + ms.m3), (ms.m3 + ms.m1), (ms.m1 + ms.m2))
     T0 = sum(abs2, ms) .- sum(abs2, thresholds)
     # 
-    ϕ(σs) = Kibble(σs, ms^2)
-    σs = thresholds .^ 2 .+ ThreeBodyDecay.polardalitz2invariants(θ, T0)
-    rmax = minimum(filter(x -> x > 0, roots(ϕ(σs))))
+	σs0 = alwaysin(ms)[end]
+	Δσs0 = Tuple(σs0) .- thresholds .^ 2
+	# 
+	Ps = 
+	    Polynomial([Δσs0[1], -cos(θ)]),
+	    Polynomial([Δσs0[2], cos(θ + π / 3)]),
+	    Polynomial([Δσs0[3], cos(θ - π / 3)])
+	# 
+    σs_P = thresholds .^ 2 .+ Ps
+	# 
+    ϕ = Base.Fix2(Kibble, ms^2)
+    rmax = minimum(filter(x -> x > 0, roots(ϕ(σs_P))))
     return rmax
 end
 
@@ -107,7 +165,7 @@ function scalledmasses(α,ms)
 end
 
 # ╔═╡ 44efb115-adde-4c67-9833-6451b9952220
-rθborder(ms) = rθdalitz.(border(ms), Ref(ms))
+rθborder(ms) = σs2rθ.(fixedborder(ms), Ref(ms))
 
 # ╔═╡ 5c0adba5-6cb3-4fe6-8f44-ebeb9cca26b4
 function rθ2σs(θr::NamedTuple{(:θ,:r)},
@@ -116,9 +174,15 @@ function rθ2σs(θr::NamedTuple{(:θ,:r)},
 	@unpack θ,r = θr
 	# 
 	thresholds = ((ms.m2 + ms.m3), (ms.m3 + ms.m1), (ms.m1 + ms.m2))
-    T0 = sum(abs2, ms) .- sum(abs2, thresholds)
 	# 
-	Ps = ThreeBodyDecay.polardalitz2invariants(θ, T0)
+	σs0 = alwaysin(ms)[end]
+	Δσs0 = Tuple(σs0) .- thresholds .^ 2
+	# 
+	Ps = 
+	    Polynomial([Δσs0[1], -cos(θ)]),
+	    Polynomial([Δσs0[2], cos(θ + π / 3)]),
+	    Polynomial([Δσs0[3], cos(θ - π / 3)])
+	# 
 	σs = thresholds .^ 2 .+ map.(Ps, r)
 	# 
 	ThreeBodyDecay.MandestamTuple(σs)
@@ -132,7 +196,7 @@ Test that the transformation ∘ inv_transformation = 1
 # ╔═╡ 89b5cf5e-20bf-4de0-9522-f019536e84b6
 begin
 	σs_i = randomPoint(ms)
-	σs_i_back = rθ2σs(rθdalitz(σs_i,ms), ms)
+	σs_i_back = rθ2σs(σs2rθ(σs_i,ms), ms)
 	@assert prod(collect(σs_i_back) .≈ collect(σs_i))
 end
 
@@ -142,8 +206,11 @@ begin
 	for α  in 0.1:0.1:0.9
 		plot!(rθborder(scalledmasses(α,ms)), proj =:polar, lims=(0, :auto))
 	end
-	scatter!(rθdalitz.([σs_i], Ref(ms)), proj =:polar, m=(6,:red))
+	scatter!(σs2rθ.([σs_i], Ref(ms)), proj =:polar, m=(6,:red))
 	plot!()
+	# 
+	scatter!(σs2rθ.(alwaysin(ms)[1:3], Ref(ms)), proj =:polar, α=0.2)
+	scatter!(σs2rθ.([alwaysin(ms)[end]], Ref(ms)), proj =:polar)
 end
 
 # ╔═╡ 55214e81-d896-4b58-b2c1-75c84ea35ffd
@@ -153,8 +220,7 @@ function σs2α(rθ, ms)
 		Kibble(rθ2σs(rθ, msα), msα^2)
 	end
 	#
-	α0 = rθ.r / rlimsdalitz(rθ.θ, ms)
-	sqrtα = nlsolve(n_ary(Kibble_α), [sqrt(α0)]).zero[1]
+	sqrtα = nlsolve(n_ary(Kibble_α), [1.0]).zero[1]
 	α = sqrtα^2
 	α > 1 && error("α = $α > 1")
 	return sqrtα^2
@@ -162,7 +228,7 @@ end
 
 # ╔═╡ 24fee2f9-c808-41e3-ba6c-aa6824fd3174
 function σs2θα(σs, ms)
-	rθ = rθdalitz(σs, ms)
+	rθ = σs2rθ(σs, ms)
 	α = σs2α(rθ, ms)
 	(; rθ.θ, α=α)
 end
@@ -170,14 +236,8 @@ end
 # ╔═╡ 1dcc0b4b-cff0-4448-bf89-e32dcd4ceda8
 data_σs = flatDalitzPlotSample(ms; Nev=100_000);
 
-# ╔═╡ efc7c156-716c-4725-b62b-76682bdd3498
-data_σs_selected = filter(data_σs) do σs
-	# rθdalitz(σs, ms).r < 10
-	true
-end;
-
 # ╔═╡ 5d88295e-5209-452c-9c9e-d2cf9eeabd32
-data_θα = σs2θα.(data_σs_selected, Ref(ms));
+data_θα = σs2θα.(data_σs, Ref(ms));
 
 # ╔═╡ 48353bd2-ddb7-4eb1-858d-ed6406b93da8
 data_xy = map(data_θα) do (θ,r)
@@ -190,12 +250,14 @@ histogram2d(data_xy, aspect_ratio=1, bins=100)
 # ╔═╡ Cell order:
 # ╠═b2323759-cbb9-4a72-ac40-46076819274f
 # ╠═edd93652-4d2d-4b50-a8f2-61eea1ea9695
-# ╠═99a30a66-2ae7-4914-9e68-b97f95dea378
+# ╠═fc40eced-a66e-4e08-ad1c-9a5dc39d4ef5
+# ╠═dc1b8aa0-1cd6-44ce-9e3f-0d56bfc269ac
 # ╠═f59fbbff-8470-4926-89c3-14dd5ab33ee8
 # ╠═4bf79a8c-8f2e-4561-b886-1964ba95a11b
 # ╠═ce4ca0e9-ee3f-47fc-a56c-570c33d61a9a
 # ╠═947586cf-63a9-4d5e-9caf-a175e3ec8bb2
 # ╠═ab731426-ce4b-4e8f-bb67-a7b9391269e0
+# ╠═823b5793-8c1a-4277-95ed-0f84faf5f088
 # ╟─3ff2fbbf-808f-4cde-ac3f-6e4ebf265df9
 # ╠═8edcf60a-f68e-4419-a45b-717ca8a071ad
 # ╟─fa2e688f-c434-4e1e-8b5e-c58cf6ccf30f
@@ -209,7 +271,6 @@ histogram2d(data_xy, aspect_ratio=1, bins=100)
 # ╠═55214e81-d896-4b58-b2c1-75c84ea35ffd
 # ╠═24fee2f9-c808-41e3-ba6c-aa6824fd3174
 # ╠═1dcc0b4b-cff0-4448-bf89-e32dcd4ceda8
-# ╠═efc7c156-716c-4725-b62b-76682bdd3498
 # ╠═5d88295e-5209-452c-9c9e-d2cf9eeabd32
 # ╠═48353bd2-ddb7-4eb1-858d-ed6406b93da8
 # ╠═736d2191-3682-4ca5-989d-099c13393132
