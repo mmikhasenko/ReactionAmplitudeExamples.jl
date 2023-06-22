4e0fb857a5a58e5dbeed47fcc81e7e737d073a77
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-06-22 10:53:55 +0200

    indevelopment

--- N-053-circledalitz.jl
+++ N-053-circledalitz.jl
@@ -15,7 +15,8 @@ begin
         Pkg.PackageSpec("Plots"),
         Pkg.PackageSpec("Parameters"),
         Pkg.PackageSpec("RecipesBase"),
-        Pkg.PackageSpec("NLsolve")
+        Pkg.PackageSpec("NLsolve"),
+		Pkg.PackageSpec("ForwardDiff")
     ])
     # 
     using RecipesBase
@@ -24,6 +25,9 @@ begin
     using Polynomials
     using Parameters
     using NLsolve
+	using ForwardDiff
+	import ForwardDiff: Dual, partials, value, gradient, jacobian
+	using LinearAlgebra
 end
 
 # ╔═╡ 0270aecb-b6c2-4d29-8760-77642194838e
@@ -42,23 +46,47 @@ theme(:wong2, frame=:box, grid=false, minorticks=true,
 # ╔═╡ f59fbbff-8470-4926-89c3-14dd5ab33ee8
 ms = ThreeBodyMasses(3, 1, 9; m0=14)
 
+# ╔═╡ 6ce6fa9b-a227-40e4-b12a-6db062078fa1
+begin
+	import ThreeBodyDecay: Invariants
+	# 
+	function Invariants(ms::NamedTuple{(:m1, :m2, :m3, :m0)}; σ1=-1.0, σ2=-1.0, σ3=-1.0)
+		sign(σ1) + sign(σ2) + sign(σ3) != 1 && error("the method works with TWO invariants given: $((σ1,σ2,σ3))")
+		
+		ms2 = ms |> collect .|> abs2
+	    σ3 < 0 && return NamedTuple{(:σ1, :σ2, :σ3)}((σ1, σ2, σ3=sum(ms2) - σ1 - σ2))
+	    σ1 < 0 && return NamedTuple{(:σ1, :σ2, :σ3)}((sum(ms2) - σ2 - σ3, σ2, σ3))
+	    return NamedTuple{(:σ1, :σ2, :σ3)}((σ1=σ1, σ2=sum(ms2) - σ3 - σ1, σ3=σ3))
+	end
+	# 
+	function _Invariants(ms::NamedTuple{(:m1, :m2, :m3, :m0)}; σ1=-1.0, σ2=-1.0, σ3=-1.0)
+		sign(σ1) + sign(σ2) + sign(σ3) != 1 && error("the method works with TWO invariants given: $((σ1,σ2,σ3))")
+		
+		ms2 = ms |> collect .|> abs2
+	    σ3 < 0 && return NamedTuple{(:σ1, :σ2, :σ3)}((σ1, σ2, σ3=sum(ms2) - σ1 - σ2))
+	    σ1 < 0 && return NamedTuple{(:σ1, :σ2, :σ3)}((sum(ms2) - σ2 - σ3, σ2, σ3))
+	    return NamedTuple{(:σ1, :σ2, :σ3)}((σ1=σ1, σ2=sum(ms2) - σ3 - σ1, σ3=σ3))
+	end
+end
+
 # ╔═╡ 823b5793-8c1a-4277-95ed-0f84faf5f088
 function alwaysin(ms)
     @unpack m0, m1, m2, m3 = ms
     # 
+	ms2 = ms |> collect .|> abs2
     σ1 = ((m2 + m3 + m0 - m1) / 2)^2
-    σ3 = σ3of1(0.1, σ1, ms^2)
-    σs2 = Invariants(ms; σ1, σ3)
+    σ3 = σ3of1(0.1, σ1, ms2)
+    σs2 = _Invariants(ms; σ1, σ3)
     # 
     σ2 = ((m3 + m1 + m0 - m2) / 2)^2
-    σ1 = σ1of2(0, σ2, ms^2)
-    σs3 = Invariants(ms; σ2, σ1)
+    σ1 = σ1of2(0, σ2, ms2)
+    σs3 = _Invariants(ms; σ2, σ1)
     # 
     σ3 = ((m1 + m2 + m0 - m3) / 2)^2
-    σ2 = σ2of3(0, σ3, ms^2)
-    σs1 = Invariants(ms; σ3, σ2)
+    σ2 = σ2of3(0, σ3, ms2)
+    σs1 = _Invariants(ms; σ3, σ2)
 
-    σs = ThreeBodyDecay.MandestamTuple(sum((σs1, σs2, σs3) .|> collect) / 3)
+    σs = NamedTuple{(:σ1, :σ2, :σ3)}(sum((σs1, σs2, σs3) .|> collect) / 3)
     [σs1, σs2, σs3, σs]
 end
 
@@ -80,23 +108,30 @@ function σs2rθ(σs, ms)
 end
 
 # ╔═╡ dc1b8aa0-1cd6-44ce-9e3f-0d56bfc269ac
-function fixedborder(ms::ThreeBodyDecay.MassTuple; Nx::Int=300)
-    # 
-    σs0 = alwaysin(ms)[end]
-    # 
-    σs_P(θ) =
-        Polynomial([σs0[1], -cos(θ)]),
-        Polynomial([σs0[2], cos(θ + π / 3)]),
-        Polynomial([σs0[3], cos(θ - π / 3)])
-    # 
-    ϕ = Base.Fix2(Kibble, ms^2)
-    rborder(θ) = minimum(filter(x -> x > 0, roots(ϕ(σs_P(θ)))))
-    σsborder(θ) =
-        map(σs_P(θ)) do P
-            P(rborder(θ))
-        end
-    θs = range(-π, π, length=Nx)
-    return ThreeBodyDecay.MandestamTuple.(σsborder.(θs))
+begin
+	σs_P(θ, σs0) =
+	    Polynomial([σs0[1], -cos(θ)]),
+	    Polynomial([σs0[2], cos(θ + π / 3)]),
+	    Polynomial([σs0[3], cos(θ - π / 3)])
+	
+	function rborder(θ, ms)
+		σs0 = alwaysin(ms)[end]
+	    # 
+	    ϕ = Base.Fix2(Kibble, ms^2)
+		return minimum(filter(x -> x > 0, roots(ϕ(σs_P(θ, σs0)))))
+	end
+	function σsborder(θ, ms)
+		σs0 = alwaysin(ms)[end]
+		# 
+	    map(σs_P(θ, σs0)) do P
+			P(rborder(θ, ms))
+	    end
+	end
+	
+	function fixedborder(ms::ThreeBodyDecay.MassTuple; Nx::Int=300)
+	    θs = range(-π, π, length=Nx)
+	    return ThreeBodyDecay.MandestamTuple.(σsborder.(θs, Ref(ms)))
+	end
 end
 
 # ╔═╡ 4bf79a8c-8f2e-4561-b886-1964ba95a11b
@@ -111,38 +146,6 @@ plot(
     plot(rθv, title="Polar dalitz plot", proj=:polar, lims=(0, :auto))
 )
 
-# ╔═╡ ab731426-ce4b-4e8f-bb67-a7b9391269e0
-function rlimsdalitz(θ, ms)
-    σs0 = alwaysin(ms)[end]
-    # 
-    σs_P =
-        Polynomial([σs0[1], -cos(θ)]),
-        Polynomial([σs0[2], cos(θ + π / 3)]),
-        Polynomial([σs0[3], cos(θ - π / 3)])
-    # 
-    ϕ = Base.Fix2(Kibble, ms^2)
-    rmax = minimum(filter(x -> x > 0, roots(ϕ(σs_P))))
-    return rmax
-end
-
-# ╔═╡ 3ff2fbbf-808f-4cde-ac3f-6e4ebf265df9
-md"""
-## $(r,\theta)$ mapping
-"""
-
-# ╔═╡ 8edcf60a-f68e-4419-a45b-717ca8a071ad
-let
-    θv = range(-π, π, length=200)
-    rv = range(0, 1, length=101)
-    zv = [rlimsdalitz(θ, ms) for r in rv, θ in θv]
-    Plots.heatmap(θv, rv, zv, proj=:polar, title="Phase-space intensity")
-end
-
-# ╔═╡ fa2e688f-c434-4e1e-8b5e-c58cf6ccf30f
-md"""
-The point of $r=0$ is not holomorphic of the scalaring transformation
-"""
-
 # ╔═╡ 7f6e8261-aba4-4d62-b6e9-4454e5a00a14
 md"""
 ## The $(\alpha,\theta)$ mapping
@@ -151,10 +154,10 @@ md"""
 # ╔═╡ 8cf51a81-aae5-4680-b900-968d0b3df5ff
 function scalledmasses(α, ms)
     m_min = minimum([ms.m1, ms.m2, ms.m3])
-    return ThreeBodyMasses(
-        m_min + α * (ms.m1 - m_min),
-        m_min + α * (ms.m2 - m_min),
-        m_min + α * (ms.m3 - m_min);
+    return (;
+        m1=m_min + α * (ms.m1 - m_min),
+        m2=m_min + α * (ms.m2 - m_min),
+        m3=m_min + α * (ms.m3 - m_min),
         m0=3m_min + α * (ms.m0 - 3m_min))
 end
 
@@ -163,7 +166,7 @@ rθborder(ms) = σs2rθ.(fixedborder(ms), Ref(ms))
 
 # ╔═╡ 5c0adba5-6cb3-4fe6-8f44-ebeb9cca26b4
 function rθ2σs(θr::NamedTuple{(:θ, :r)},
-    ms::ThreeBodyDecay.MassTuple)
+    ms::NamedTuple{(:m1, :m2, :m3, :m0)})
     #
     @unpack θ, r = θr
     # 
@@ -176,7 +179,7 @@ function rθ2σs(θr::NamedTuple{(:θ, :r)},
     # 
     σs = map.(Ps, r)
     # 
-    ThreeBodyDecay.MandestamTuple(σs)
+    NamedTuple{(:σ1, :σ2, :σ3)}(σs)
 end
 
 # ╔═╡ 0f94bc6d-0084-4136-8613-de48bc877149
@@ -204,39 +207,137 @@ begin
     scatter!(σs2rθ.([alwaysin(ms)[end]], Ref(ms)), proj=:polar)
 end
 
-# ╔═╡ 55214e81-d896-4b58-b2c1-75c84ea35ffd
-function σs2α(rθ, ms)
-    function Kibble_α(sqrtα)
-        msα = scalledmasses(sqrtα^2, ms)
-        Kibble(rθ2σs(rθ, msα), msα^2)
-    end
-    #
-    sqrtα = nlsolve(n_ary(Kibble_α), [1.0]).zero[1]
-    α = sqrtα^2
-    α > 1 && error("α = $α > 1")
-    return sqrtα^2
+# ╔═╡ fcf282e9-3c30-4ccb-99e0-f8c8d5ef0d8c
+md"""
+## Transformation to alpha
+"""
+
+# ╔═╡ 0124a4d4-4aa3-4875-b47f-eb3c2cada230
+function Kibble_α(sqrtα, x_rθ, ms)
+    msα = scalledmasses(sqrtα^2, ms)
+	rθ = NamedTuple{(:θ,:r)}(x_rθ)
+    Kibble(rθ2σs(rθ, msα), msα |> collect .|> abs2)
 end
 
-# ╔═╡ 24fee2f9-c808-41e3-ba6c-aa6824fd3174
-function σs2θα(σs, ms)
+# ╔═╡ 33eb8faa-e1b0-4126-bc47-6e2aee77c44d
+rθ2sqrtα(x_rθ, ms) = nlsolve(x->Kibble_α(x[1], x_rθ, ms), [1.0]).zero[1]
+
+# ╔═╡ 86015c97-0ecd-4196-9636-35ad4129c753
+function rθ2sqrtα(d::Vector{Dual{T,V,N}}, ms) where {T,V,N}
+	x0 = value.(d)
+	y0 = rθ2sqrtα(x0, ms)
+	# 
+	dfdy = ForwardDiff.derivative(sqrtα -> Kibble_α(sqrtα, x0, ms), y0)
+    dfdx = ForwardDiff.gradient(x->Kibble_α(y0, x, ms), x0)
+    dydx = -inv(dfdy) * dfdx
+	# 
+	nt = sum(i->dydx[i]*partials.(d, i), 1:N)
+	Dual{T}(y0, Tuple(nt))
+end
+
+# ╔═╡ aa661d42-5273-4f63-a71a-795a20d1da14
+function σs2ζχ(σs, ms)
     rθ = σs2rθ(σs, ms)
-    α = σs2α(rθ, ms)
-    (; rθ.θ, α=α)
+	x_rθ = rθ |> collect
+	# 
+	sqrtα = rθ2sqrtα(x_rθ, ms)
+    α = sqrtα^2
+    α > 1 && error("α = $α > 1")
+	# 
+	θ = rθ.θ
+    [α*cos(θ), α*sin(θ)]
+end
+
+# ╔═╡ af7c35b6-971f-4860-811b-4a6b9acb4017
+function ζχ2σs(ζχ, ms)
+	ζ,χ = ζχ
+	α = norm(ζχ)
+	α > 1 && error("α = $α > 1") 
+	θ = atan(χ,ζ)
+	# 
+	σs0 = alwaysin(ms)[end]
+	r = rborder(θ, scalledmasses(α, ms))
+	# 
+	σsv = map(σs_P(θ, σs0)) do P
+		P(r)
+	end
+	return NamedTuple{(:σ1, :σ2, :σ3)}(σsv)
+end
+
+# ╔═╡ aab3ddac-fae2-4a68-afe4-b8828df3c237
+ζχ2σs(σs2ζχ(σs_i, ms), ms), σs_i
+
+# ╔═╡ 25c6c066-e511-459a-990f-7d5063f23483
+σs2ζχ_jacobian(σs, ms) = 
+	ForwardDiff.jacobian(collect(σs)[1:2]) do σ1σ2
+		σs2ζχ(_Invariants(ms; NamedTuple{(:σ1, :σ2)}(σ1σ2)...), ms)
+	end
+
+# ╔═╡ 58d6ef6b-855c-4057-916d-97033c102b2f
+σs2ζχ_jacobian(σs_i, ms)
+
+# ╔═╡ 4ecc9880-5418-4f3a-95a3-1b69e673a97c
+ForwardDiff.gradient([110.0, 160]) do σ1σ2
+		σs2ζχ(_Invariants(ms; σ2 = σ1σ2[2], σ1=σ1σ2[1]), ms)[1]
+	end
+
+# ╔═╡ 14c7cdd4-aaef-4259-a263-7ef9f309e4cd
+ForwardDiff.gradient([110.0, 160]) do σ1σ2
+		σs2ζχ(_Invariants(ms; σ2 = σ1σ2[2], σ1=σ1σ2[1]), ms)[2]
+	end
+
+# ╔═╡ 7e174855-5875-48ea-b51a-227d13b9fb5f
+let ϵ = 1e-6
+	xxx(σ1σ2) = σs2ζχ(_Invariants(ms; σ1=σ1σ2[1], σ2 = σ1σ2[2]), ms)
+	hcat(
+		(xxx([110+ϵ,160]) - xxx([110-ϵ,160])) / 2ϵ,
+		(xxx([110,160+ϵ]) - xxx([110,160-ϵ])) / 2ϵ
+	)
+end
+
+# ╔═╡ 3d44e28e-ba17-46a0-ba37-5d57ff61c5a7
+σs2ζχ_jacobian(σs_i, ms)
+
+# ╔═╡ 6300912a-3e8e-4868-9944-1e7bd64cb5b9
+ζχ_jacobian(ζχ, ms) = det(σs2ζχ_jacobian(ζχ2σs(ζχ, ms), ms))
+
+# ╔═╡ 22378478-cf42-49c0-aa9b-557e3f036d7e
+ζχ_jacobian([0.2,0.3], ms)
+
+# ╔═╡ 891df957-d9ac-4ef7-a0f5-dd9d0167fc0a
+norm([0.3,0.4])
+
+# ╔═╡ a2efb67a-2551-490d-b36b-dc2fb1816bdb
+let
+	xv = range(-1,1, 30)
+	yv = range(-1,1, 30)
+	calv = [norm([x,y]) > 1 ? NaN : ζχ_jacobian([x,y], ms) for x in xv, y in yv]
+	heatmap(xv,yv,calv, aspect_ratio=1)
+end
+
+# ╔═╡ 55214e81-d896-4b58-b2c1-75c84ea35ffd
+begin
+	function σs2α(rθ, ms)
+	    sqrtα = nlsolve(x->Kibble_α(x[1], rθ, ms), [1.0]).zero[1]
+	    α = sqrtα^2
+	    α > 1 && error("α = $α > 1")
+	    return sqrtα^2
+	end
+	function σs2θα(σs, ms)
+	    rθ = σs2rθ(σs, ms)
+	    α = σs2α(rθ, ms)
+	    (; rθ.θ, α=α)
+	end
 end
 
 # ╔═╡ 1dcc0b4b-cff0-4448-bf89-e32dcd4ceda8
-data_σs = flatDalitzPlotSample(ms; Nev=100_000);
+data_σs = flatDalitzPlotSample(ms; Nev=10_000);
 
-# ╔═╡ 5d88295e-5209-452c-9c9e-d2cf9eeabd32
-data_θα = σs2θα.(data_σs, Ref(ms));
+# ╔═╡ d0dc58cc-d818-4e8e-b961-6d39bea67691
+data_ζχ = Tuple.(σs2ζχ.(data_σs, Ref(ms)))
 
-# ╔═╡ 48353bd2-ddb7-4eb1-858d-ed6406b93da8
-data_xy = map(data_θα) do (θ, r)
-    (r * cos(θ), r * sin(θ))
-end;
-
-# ╔═╡ 736d2191-3682-4ca5-989d-099c13393132
-histogram2d(data_xy, aspect_ratio=1, bins=100)
+# ╔═╡ d0e90d94-f209-4473-93b3-2fc20ead1e68
+histogram2d(data_ζχ, aspect_ratio=1, bins=100)
 
 # ╔═╡ Cell order:
 # ╟─0270aecb-b6c2-4d29-8760-77642194838e
@@ -248,11 +349,8 @@ histogram2d(data_xy, aspect_ratio=1, bins=100)
 # ╠═4bf79a8c-8f2e-4561-b886-1964ba95a11b
 # ╠═ce4ca0e9-ee3f-47fc-a56c-570c33d61a9a
 # ╠═947586cf-63a9-4d5e-9caf-a175e3ec8bb2
-# ╠═ab731426-ce4b-4e8f-bb67-a7b9391269e0
+# ╠═6ce6fa9b-a227-40e4-b12a-6db062078fa1
 # ╠═823b5793-8c1a-4277-95ed-0f84faf5f088
-# ╟─3ff2fbbf-808f-4cde-ac3f-6e4ebf265df9
-# ╠═8edcf60a-f68e-4419-a45b-717ca8a071ad
-# ╟─fa2e688f-c434-4e1e-8b5e-c58cf6ccf30f
 # ╟─7f6e8261-aba4-4d62-b6e9-4454e5a00a14
 # ╠═8cf51a81-aae5-4680-b900-968d0b3df5ff
 # ╠═44efb115-adde-4c67-9833-6451b9952220
@@ -260,9 +358,24 @@ histogram2d(data_xy, aspect_ratio=1, bins=100)
 # ╠═5c0adba5-6cb3-4fe6-8f44-ebeb9cca26b4
 # ╟─0f94bc6d-0084-4136-8613-de48bc877149
 # ╠═89b5cf5e-20bf-4de0-9522-f019536e84b6
+# ╟─fcf282e9-3c30-4ccb-99e0-f8c8d5ef0d8c
+# ╠═0124a4d4-4aa3-4875-b47f-eb3c2cada230
+# ╠═33eb8faa-e1b0-4126-bc47-6e2aee77c44d
+# ╠═86015c97-0ecd-4196-9636-35ad4129c753
+# ╠═aa661d42-5273-4f63-a71a-795a20d1da14
+# ╠═af7c35b6-971f-4860-811b-4a6b9acb4017
+# ╠═aab3ddac-fae2-4a68-afe4-b8828df3c237
+# ╠═25c6c066-e511-459a-990f-7d5063f23483
+# ╠═58d6ef6b-855c-4057-916d-97033c102b2f
+# ╠═4ecc9880-5418-4f3a-95a3-1b69e673a97c
+# ╠═14c7cdd4-aaef-4259-a263-7ef9f309e4cd
+# ╠═7e174855-5875-48ea-b51a-227d13b9fb5f
+# ╠═3d44e28e-ba17-46a0-ba37-5d57ff61c5a7
+# ╠═6300912a-3e8e-4868-9944-1e7bd64cb5b9
+# ╠═22378478-cf42-49c0-aa9b-557e3f036d7e
+# ╠═891df957-d9ac-4ef7-a0f5-dd9d0167fc0a
+# ╠═a2efb67a-2551-490d-b36b-dc2fb1816bdb
 # ╠═55214e81-d896-4b58-b2c1-75c84ea35ffd
-# ╠═24fee2f9-c808-41e3-ba6c-aa6824fd3174
 # ╠═1dcc0b4b-cff0-4448-bf89-e32dcd4ceda8
-# ╠═5d88295e-5209-452c-9c9e-d2cf9eeabd32
-# ╠═48353bd2-ddb7-4eb1-858d-ed6406b93da8
-# ╠═736d2191-3682-4ca5-989d-099c13393132
+# ╠═d0dc58cc-d818-4e8e-b961-6d39bea67691
+# ╠═d0e90d94-f209-4473-93b3-2fc20ead1e68
